549816f66db90ac0acb5dd4eff0229795728b3cc
feghalilara <feghali.lara@outlook.com>
2024-03-16 23:45:35 +0100

    remove commented code

--- base_de_donnees/filter_csv.py
+++ base_de_donnees/filter_csv.py
@@ -1,16 +1,11 @@
 import pandas as pd
 
 def filter_csvs_create_hotels():
-    # hotel_bookings_columns = ["hotel", "is_canceled", "reservation_status_date", "country", "deposit_type", "reserved_room_type", "assigned_room_type", "adults", "children", "stays_in_week_nights","stays_in_weekend_nights", "booking_changes", "adr", "reservation_status", "market_segment"]
-    # hotel_reviews_columns = ["hotel", "zip_code", "numRev", "Score", "Cleanliness", "Comfort"]
-
-    # hotel_bookings_file = "hotel_bookings.csv"
     ind_all_file = "ind_all.csv"
     nyc_all_file = "nyc_all.csv"
     la_all_file = "la_all.csv"
     ol_all_file = "ol_all.csv"
 
-    # hotel_bookings_df = pd.read_csv(hotel_bookings_file)
     ind_all_df = pd.read_csv(ind_all_file)
     nyc_all_df = pd.read_csv(nyc_all_file)
     la_all_df = pd.read_csv(la_all_file)
@@ -41,16 +36,8 @@ def filter_csvs_create_hotels():
 
     hotels_df = pd.concat([la_hotels_df, nyc_hotels_df, ol_hotels_df, ind_hotels_df], ignore_index=True)
     hotels_df['id'] = range(1, len(hotels_df) + 1)
-    # hotel_bookings_filtered = hotel_bookings_df[hotel_bookings_columns]
-    # nyc_all_filtered = nyc_all_df[hotel_reviews_columns]
-    # la_all_filtered = la_all_df[hotel_reviews_columns]
-    # ol_all_filtered = ol_all_df[hotel_reviews_columns]
 
-    # hotel_bookings_filtered.to_csv("hotel_bookings_filtered.csv", index=False)
     hotels_df.to_csv("hotels.csv", index=False)
-    # nyc_all_filtered.to_csv("nyc_all_filtered.csv", index=False)
-    # la_all_filtered.to_csv("la_all_filtered.csv", index=False)
-    # ol_all_filtered.to_csv("ol_all_filtered.csv", index=False)
 
     print("created hotels.csv")
 
